65b7062545d2a0fb5ee8eb5f12abaaaac63f41af
carloshermoso64 <59064594+carloshermoso64@users.noreply.github.com>
2020-04-17 16:29:53 +0200

    User test working

--- src/test/java/edu/upc/dsa/UsuarioTest.java
+++ src/test/java/edu/upc/dsa/UsuarioTest.java
@@ -13,15 +13,17 @@ public class UsuarioTest {
     public void setUp() {
         gm = GameManagerImp.getInstance();
 
-        this.gm.addUsuario("Marc","Vila","marclays");
-        this.gm.addUsuario("Toni", "Norton", "tonilivo");
-        this.gm.addUsuario("Ferran", "Lopez", "elbicho");
+        this.gm.addUsuario("Carlos", "Hermoso", "seko");
+        this.gm.addUsuario("Anas","Amlou","3azzy");
+        this.gm.addUsuario("Juninho", "Penambucano", "ORey");
+
+
     }
 
     @Test //Adding users + checking the number of users
     public void testAddUsers() {
 
-        this.gm.addUsuario("Juan", "Magan", "lapulga");
+        this.gm.addUsuario("Juan", "Palomo", "fenomeno");
 
         Assert.assertEquals(4, this.gm.getNumUsuario());
     }
@@ -29,21 +31,21 @@ public class UsuarioTest {
     @Test
     public void testAddObjects(){
 
-        this.gm.addObjetoUsuario("marclays","Espada", "Espada gigante");
-        this.gm.addObjetoUsuario("marclays","Arco", "Arco magnifico");
-        this.gm.addObjetoUsuario("tonilivo", "Escudo", "Escudo gigante");
+        this.gm.addObjetoUsuario("seko","coche", "rojo");
+        this.gm.addObjetoUsuario("seko","moto", "amarillo");
+        this.gm.addObjetoUsuario("3azzy", "avion", "verde");
 
-        Assert.assertEquals(2, this.gm.getNumObjetosbyUsuario("marclays"));
-        Assert.assertEquals(1,this.gm.getNumObjetosbyUsuario("tonilivo"));
+        Assert.assertEquals(2, this.gm.getNumObjetosbyUsuario("seko"));
+        Assert.assertEquals(1,this.gm.getNumObjetosbyUsuario("3azzy"));
     }
 
     @Test
     public void testModifyUser(){
 
-        this.gm.addObjetoUsuario("elbicho", "moneda", "moneda brillante");
-        this.gm.updateUsuario("Ferran","Lopez","fr98@yahoo.com", "elbicho");
-        Assert.assertEquals("fr98@yahoo.com",this.gm.getUsuario("elbicho").getMail());
-        Assert.assertEquals(1,this.gm.getNumObjetosbyUsuario("elbicho"));
+        this.gm.addObjetoUsuario("seko", "coche", "amarillo");
+        this.gm.updateUsuario("seko","hermoso","seko@gmail.com", "seko");
+        Assert.assertEquals("seko@gmail.com",this.gm.getUsuario("seko").getMail());
+        Assert.assertEquals(1,this.gm.getNumObjetosbyUsuario("seko"));
     }
 
     @After
